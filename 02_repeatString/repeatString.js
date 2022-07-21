3de1493d70748c7303273c9447ccc341ee17c4f9
John <johntony366@gmail.com>
2022-07-21 17:11:21 +0530

    Solve q2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, n) {
+    if (n < 0) {
+        return 'ERROR';
+    }
+    let str = "";
+    while (n-- > 0) {
+        str += string;
+    }
+    return str;
 };
 
 // Do not edit below this line
