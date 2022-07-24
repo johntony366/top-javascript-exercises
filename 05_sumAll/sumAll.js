2fce0fcec5af1866b2feac090a36ed94f51a4ae3
John <johntony366@gmail.com>
2022-07-24 20:45:12 +0530

    Solve 3-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function (n1, n2) {
+    if (!(Number.isInteger(n1) && Number.isInteger(n2)) || (n1 < 0 || n2 < 0)) {
+        return 'ERROR';
+    }
+    let start = n1 <= n2 ? n1 : n2;
+    const end = n2 >= n1 ? n2 : n1;
+    let result = 0;
+    while (start <= end) {
+        result += start;
+        ++start;
+    }
+    return result;
 
 };
+
+//1 + 
 
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //If year is divisible by 100, it should also be divisible by 400
+    //Otherwise it should be divisible by 4
+    if (year % 100 == 0) {
+        return (year % 400 == 0);
+    }
+    return (year % 4 == 0);
 };
 
 // Do not edit below this line
